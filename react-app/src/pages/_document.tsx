3a5a61f1c466a365024d9cb93778e3360364dc77
DGangl <danielgangl9@gmail.com>
2022-09-11 14:31:09 +0200

    feat: inital commit

---
+++ react-app/src/pages/_document.tsx
@@ -0,0 +1,41 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang='en' className='h-full  bg-gray-100'>
+        <Head>
+          <link
+            rel='preload'
+            href='/fonts/inter-var-latin.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+        </Head>
+        <body className='h-full'>
+          <Main />
+          <NextScript />
+          <style global>{`
+            #__next {
+              height: 100%;
+            }
+          `}</style>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
