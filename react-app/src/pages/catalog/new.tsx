4ace9ac6af8e4e0f27a335c59628c2103395ceb0
danielgangl <danielgangl9@gmail.com>
2022-09-20 13:52:46 +0200

    feat: enhance create product form

--- react-app/src/pages/catalog/new.tsx
+++ react-app/src/pages/catalog/new.tsx
@@ -1,18 +1,19 @@
+import Link from 'next/link';
+
 import Button from '@/components/buttons/Button';
 import Dropdown from '@/components/form-controls/Dropdown';
 import Input from '@/components/form-controls/Input';
+import Textarea from '@/components/form-controls/Textarea';
 import AppLayout from '@/components/layout/AppLayout';
-import Link from 'next/link';
 
 export default function NewProduct() {
   return (
     <AppLayout>
-      <div className='mx-auto max-w-7xl px-4 sm:px-6 md:px-8'>
+      <div className='mx-auto w-4/6 p-8 px-4 sm:px-6 md:px-8'>
         <h1 className='text-2xl font-semibold text-gray-900'>New Product</h1>
-      </div>
-      <div className='mx-auto max-w-7xl px-4 sm:px-6 md:px-8'>
+
         <div className='py-4'>
-          <div className='flex h-96 flex-col justify-between rounded-lg border-2 border-solid border-gray-200 py-4'>
+          <div className='flex flex-col justify-between rounded-lg border-2 border-solid border-gray-200 py-4'>
             <div className='m-6 grid grid-cols-6 gap-y-4 gap-x-8'>
               <div className='col-span-3'>
                 <Input
@@ -27,7 +28,6 @@ export default function NewProduct() {
                 <Dropdown
                   name='Country of Sale'
                   options={['United States']}
-                  disabled={true}
                 ></Dropdown>
               </div>
               <div className='col-span-3'>
@@ -36,8 +36,32 @@ export default function NewProduct() {
                   options={['Clothing', 'Sports Wear']}
                 ></Dropdown>
               </div>
+
+              <div className='col-span-6 h-28'>
+                <Textarea
+                  name='Description'
+                  placeholder='Describe your product'
+                ></Textarea>
+              </div>
             </div>
-            <div className='flex justify-center'>
+            <div className='m-6 grid grid-cols-6 gap-y-4 gap-x-8'>
+              <div className='col-span-3'>
+                <Dropdown
+                  name='Color'
+                  options={['White', 'Yellow', 'Black']}
+                ></Dropdown>
+              </div>
+              <div className='col-span-3'>
+                <Dropdown
+                  name='Gender'
+                  options={['Unisex', 'Men', 'Women']}
+                ></Dropdown>
+              </div>
+              <div className='col-span-6'>
+                <Input name='Size' placeholder='M'></Input>
+              </div>
+            </div>
+            <div className='flex justify-center py-4'>
               <Link href='/catalog'>
                 <Button>Create Product</Button>
               </Link>
