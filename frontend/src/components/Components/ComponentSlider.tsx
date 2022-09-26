eac4911a02b5979b920a8e9a00b84488556cff0f
DGangl <danielgangl9@gmail.com>
2022-09-26 10:30:39 +0200

    feat: add sponsor logos

--- frontend/src/components/Components/ComponentSlider.tsx
+++ frontend/src/components/Components/ComponentSlider.tsx
@@ -6,6 +6,11 @@ import img3 from "images/collections/4.png";
 import img4 from "images/collections/3.png";
 //
 import Tableland from "images/Sponsors/Tableland.svg";
+import Spruce from "images/Sponsors/Spruce.svg";
+import TheGraph from "images/Sponsors/TheGraph.svg";
+import IPFS from "images/Sponsors/IPFS.svg";
+import Polygon from "images/Sponsors/Polygon.svg";
+import Ceramic from "images/Sponsors/Ceramic.svg";
 
 import CardCategory3, {
   CardCategory3Props,
@@ -16,31 +21,31 @@ export const TechStack: CardCategory3Props[] = [
   {
     name: "Sponsor",
     desc: "Ceramic",
-    featuredImage: img1,
+    featuredImage: Ceramic,
     color: "bg-slate-100/80",
   },
   {
     name: "Sponsor",
     desc: "The Graph",
-    featuredImage: img2,
+    featuredImage: TheGraph,
     color: "bg-slate-100/80",
   },
   {
     name: "Sponsor",
     desc: "IPFS",
-    featuredImage: img3,
+    featuredImage: IPFS,
     color: "bg-slate-100/80",
   },
   {
     name: "Sponsor",
     desc: "Polygon",
-    featuredImage: img4,
+    featuredImage: Polygon,
     color: "bg-slate-100/80",
   },
   {
     name: "Sponsor",
     desc: "Spruce",
-    featuredImage: img4,
+    featuredImage: Spruce,
     color: "bg-slate-100/80",
   },
   {
@@ -92,9 +97,7 @@ const ComponentSlider = () => {
 
   return (
     <div className={`${UNIQUE_CLASS}`}>
-      <Heading
-        className="mb-12 lg:mb-14 text-[#1d1e5f] dark:text-neutral-50 nc-p-r-container "
-      >
+      <Heading className="mb-12 lg:mb-14 text-[#1d1e5f] dark:text-neutral-50 nc-p-r-container ">
         Explore our tech stack.
       </Heading>
       <div className="" data-glide-el="track">
